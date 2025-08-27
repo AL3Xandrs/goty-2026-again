17bc2b399e0f95677cfd02ab18e6df3f0977a1d8
Citronn <citron440@gmail.com>
2025-08-27 12:17:36 +0300

    couple tweaks
    We are currently trying a new approach to how we manage our repository which goes by the philosophy "generate your uid's and push them".
    This should fix all of our issues with how github manages files.
    If i see one more merge conflict i am going to be very very angy!

---
+++ blackjack/player_hand.gd
@@ -0,0 +1,91 @@
+class_name PlayerNode
+extends Node2D
+
+const VALUE: int = 0
+const SUIT: int = 1
+const WEIGHT: int = 2
+
+var playerHand: Array
+@onready var cardScene = load("res://blackjack/card_scene.tscn")
+@onready var cardStack := $"../CardStackNode"
+
+
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_PAUSABLE
+	playerHand.clear()
+
+func hit() -> void:
+	
+	var newCard := _generateWeightedCard()
+	playerHand.append(newCard)
+	drawCard(newCard)
+	await get_tree().create_timer(0.5,false).timeout
+	
+
+func getScore() -> int:
+	var sum:int = 0
+	var aceCount: int = 0
+	for card in playerHand:
+		var currentValue: int = card[VALUE]
+		sum += min(10, currentValue) # face din face cards in 10 ca la blackjack wow ce chestie
+		if currentValue == 1: aceCount+=1
+	for i in range(aceCount):
+		if sum + 10 <= 21:
+			sum += 10 # face asii automat mari daca se poate
+	return sum
+	
+
+func _generateWeightedCard() -> Array:
+	var finalCard: Array
+	var drawnCards: Array
+	var handScore: int = getScore()
+	var maxWeight: int = -99
+	
+	for i in range(5): # cu avantaj :alien:
+		var newCard := _generateRandomCard()
+		#while newCard not in playerHand:     # to no thave repeats
+		#	newCard = _generateRandomCard()   # ts crashes godot xdd xd
+		var weight: int = handScore + newCard[VALUE]
+		if weight > 21:
+			weight = -1
+		if weight > maxWeight:
+			maxWeight = weight
+		newCard.append(weight)
+		drawnCards.append(newCard)
+	
+	for card in drawnCards:
+		if card[WEIGHT] == maxWeight:
+			finalCard = [card[VALUE], card[SUIT]]
+			break
+	
+	return finalCard
+
+
+
+
+func _generateRandomCard() -> Array:
+	var newCard: Array
+	newCard.append(randi_range(1, 13))
+	var newSuit: String = ["Red", "Blue", "Green", "Yellow"][randi_range(0,3)]
+	newCard.append(newSuit)
+	return newCard
+
+
+func drawCard(card: Array):
+	var cardInstance = cardScene.instantiate()
+	var startPosition = cardStack.position - position
+	var finalPosition = Vector2(playerHand.size() * 40, 0)
+	cardInstance.startPosition = startPosition
+	cardInstance.finalPosition = finalPosition
+	cardInstance.value = card[VALUE]
+	cardInstance.suit = card[SUIT]
+	add_child.call_deferred(cardInstance)
+	
+
+func reset():
+	for node in get_children():
+		node.discard()
+	playerHand.clear()
+func isBusted() ->bool:
+	return (getScore() > 21)
+	
